39dc8fb80d3ab6619fac2cddc4f6127d7cbd5f05
Tomasz Pałucki <tpalucki@gmail.com>
2020-07-24 18:46:33 +0200

    Added Java Mail Bean + implementatnion for mail sending.

--- src/main/java/io/github/tpalucki/grammati/controller/SubscriptionController.java
+++ src/main/java/io/github/tpalucki/grammati/controller/SubscriptionController.java
@@ -1,6 +1,8 @@
 package io.github.tpalucki.grammati.controller;
 
 import io.github.tpalucki.grammati.domain.Subscription;
+import io.github.tpalucki.grammati.service.EmailService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller("/")
+@RequiredArgsConstructor
 public class SubscriptionController {
+
+    private final EmailService emailService;
 
     @GetMapping
     public String subscribeForm(Model model) {
@@ -20,8 +25,7 @@ public class SubscriptionController {
 
     @PostMapping
     public String processSubscription(@Validated Subscription subscription) {
-
-
+        emailService.sendConfirmationEmail(subscription.getEmail());
         return "confirmPage";
     }
 }
--- src/main/java/io/github/tpalucki/grammati/service/EmailService.java
+++ src/main/java/io/github/tpalucki/grammati/service/EmailService.java
@@ -1,8 +1,24 @@
 package io.github.tpalucki.grammati.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EmailService {
 
+    private final JavaMailSender mailSender;
+
+    // TODO fill the email properly
+    public void sendConfirmationEmail(String email) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@grammati.com");
+        message.setTo(email);
+        message.setSubject("Confirm your email");
+        message.setText("This is the test email template for your email:\n%s\n");
+
+        mailSender.send(message);
+    }
 }
