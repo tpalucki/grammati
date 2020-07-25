573f196eb5878315e82370c53e7c1d66e9d9a101
Tomasz Pałucki <tpalucki@gmail.com>
2020-07-25 18:12:17 +0200

    Added Excercise Respository interace

---
+++ src/main/java/io/github/tpalucki/grammati/repository/ExcerciseRespository.java
@@ -0,0 +1,8 @@
+package io.github.tpalucki.grammati.repository;
+
+import io.github.tpalucki.grammati.domain.Excercise;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ExcerciseRespository extends CrudRepository<Excercise, Long> {
+
+}
