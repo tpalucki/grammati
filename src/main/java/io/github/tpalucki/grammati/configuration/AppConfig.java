3760943d7346341328d9f94f374a13020923cfdc
Tomasz Pałucki <tpalucki@gmail.com>
2020-11-08 15:41:56 +0100

    Added CORS configuration.

--- src/main/java/io/github/tpalucki/grammati/configuration/AppConfig.java
+++ src/main/java/io/github/tpalucki/grammati/configuration/AppConfig.java
@@ -4,12 +4,21 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @Getter
 @PropertySource("application.properties")
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     @Value("${app.title}")
     private String appTitle;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+    }
+
 }
