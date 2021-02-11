373d984aafd5da68132f4a69e6f2801d27811b74
Tomasz Pałucki <tpalucki@gmail.com>
2021-02-11 21:48:11 +0100

    Schema update.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,10 +7,17 @@ VALUES ('abc'),
 INSERT INTO question (question)
 VALUES ('How are you?'),
        ('How much do you weight?'),
-       ('How high are you?');
+       ('How high are you?'),
+       ('What are the odds?');
 
 INSERT INTO answer (answer_text, correct, question_id)
 VALUES ('Fine, thank you', TRUE, 1),
        ('Not so bad', FALSE, 1),
-       ('Could be better, nut thanks', FALSE, 1),
-       ('I am 1.8m tall', FALSE, 1);
+       ('Could be better, nut thanks', TRUE, 2),
+       ('I am 1.8m tall', FALSE, 2);
+
+INSERT INTO quiz_to_questions (quiz_id, question_id)
+VALUES (1, 1),
+       (1, 2),
+       (2, 3),
+       (2, 4);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,7 @@
+DROP TABLE IF EXISTS quiz_to_questions;
 DROP TABLE IF EXISTS answer;
 DROP TABLE IF EXISTS question;
 DROP TABLE IF EXISTS quiz;
-
-CREATE TABLE quiz
-(
-    quiz_id    INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    session_id VARCHAR
-);
 
 -- Question is not connected to quiz directly, question will be rather random for the quiz
 CREATE TABLE question
@@ -24,4 +19,20 @@ CREATE TABLE answer
     correct     BOOLEAN,
     question_id INTEGER,
     CONSTRAINT fk_question FOREIGN KEY (question_id) REFERENCES question (question_id)
+);
+
+
+CREATE TABLE quiz
+(
+    quiz_id    INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    session_id VARCHAR
+);
+
+CREATE TABLE quiz_to_questions
+(
+    quiz_to_questions_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    quiz_id              INTEGER NOT NULL,
+    question_id          INTEGER NOT NULL,
+    CONSTRAINT fk_question FOREIGN KEY (question_id) REFERENCES question (question_id),
+    CONSTRAINT fk_quiz FOREIGN KEY (quiz_id) REFERENCES quiz (quiz_id)
 );
