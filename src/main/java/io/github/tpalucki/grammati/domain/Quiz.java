3786b35b372be08f2e46c50b2d4ef9170f858b99
Tomasz Pałucki <tpalucki@gmail.com>
2021-02-19 00:19:33 +0100

    Changes: - Question is generated with one question. Mail is sent. Link generated as well. When user clicks link it is redirected to quiz resource in the client app.

--- src/main/java/io/github/tpalucki/grammati/domain/Quiz.java
+++ src/main/java/io/github/tpalucki/grammati/domain/Quiz.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class Quiz {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long quizId;
     private String sessionId;
 
