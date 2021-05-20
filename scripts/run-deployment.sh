458aee4acb6383326edb2309910e21489f5c1f17
Tomasz Pałucki <tpalucki@gmail.com>
2021-05-20 18:18:01 +0200

    Changes: - Deployment scripts updates.
    Signed-off-by: Tomasz Pałucki <tpalucki@gmail.com>

--- scripts/run-deployment.sh
+++ scripts/run-deployment.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-export DEPLOYMENT_DIR=tmp/
+export DEPLOYMENT_DIR=deployment/
+export APP_VERSION=0.0.3
 
 cd ${DEPLOYMENT_DIR}
 
-docker load --input io.github.tpalucki_gramilka-backend_0.0.3.tar
-docker load --input io.github.tpalucki_gramilka-frontend_0.0.3.tar
+docker load --input io.github.tpalucki_gramilka-backend_${APP_VERSION}.tar
+docker load --input io.github.tpalucki_gramilka-frontend_${APP_VERSION}.tar
